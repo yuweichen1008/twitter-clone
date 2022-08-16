b7972f19d7218c4467079dfedd3a4ad9d303e557
yuweichen1008 <yuweiichen@gmail.com>
2022-08-17 07:56:58 +0800

    firebase image upload feature

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
-import { getFirestore } from "firebase/firebase"
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,7 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
-const db  = getFirestore()
+const db  = getFirestore(app);
 const storage = getStorage()
 
 export{ app, db, storage };
